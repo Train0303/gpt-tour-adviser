dbf4dd42d8c6021352aa5fa2c3c0203216daf67e
건부기 <pigonhairtt@gmail.com>
2023-05-30 19:48:49 +0900

    feat: apply test Python file in Java (#4)
    - 파이썬 소스 파일을 자바에서 실행 가능한 환경을 구현(CommandController)

--- backEnd/src/main/java/com/gptTour/backEnd/controller/CommandController.java
+++ backEnd/src/main/java/com/gptTour/backEnd/controller/CommandController.java
@@ -1,26 +1,58 @@
 package com.gptTour.backEnd.controller;
 
-import org.python.util.PythonInterpreter;
+import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.StringWriter;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.Executors;
+import java.util.function.Consumer;
+
 
 @RestController
 public class CommandController {
 
-    private static PythonInterpreter interpreter;
+    private static class StreamGobbler implements Runnable {
+        private InputStream inputStream;
+        private Consumer<String> consumer;
+
+        public StreamGobbler(InputStream inputStream, Consumer<String> consumer) {
+            this.inputStream = inputStream;
+            this.consumer = consumer;
+        }
+
+        @SneakyThrows()
+        @Override
+        public void run() {
+            new BufferedReader(new InputStreamReader(inputStream, "CP949")).lines().forEach(consumer);
+        }
+    }
 
     @GetMapping("/pytest")
-    public void pytest() {
-        interpreter = new PythonInterpreter();
-        StringWriter out = new StringWriter();
-        interpreter.setOut(out);
-        interpreter.execfile("D:/git/gpt-tour-adviser/csv_generator/openai_test.py");
+    public void pytest() throws IOException, InterruptedException {
+        System.out.println(":: START :: User ProcessBuilder ");
+        String homeDirectory = System.getProperty("user.home");
+        System.out.println(":: homeDirectory is " + homeDirectory);
 
-        String result = out.toString();
-        System.out.println(result);
-        interpreter.close();
+        ProcessBuilder builder = new ProcessBuilder();
+        builder.directory(new File(homeDirectory));
+
+        boolean isWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
+        System.out.println(":: OS is " + (isWindows ? "window" : "linux"));
+
+        if(isWindows) {
+            builder.command("cmd.exe", "/c", "python D:/git/gpt-tour-adviser/csv_generator/test.py");
+        } else {
+            builder.command("sh", "-c", "ls -l | grep P");
+        }
+
+        Process process = builder.start();
+        StreamGobbler streamGobbler = new StreamGobbler(process.getInputStream(), System.out::println);
+        Executors.newSingleThreadExecutor().submit(streamGobbler);
+
+        int exitCode = process.waitFor();
+        assert exitCode == 0;
     }
 
 }
